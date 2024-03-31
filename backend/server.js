7b3b1bdce50e2bb2de890f2a79c75b0bbeb2daaf
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2024-03-31 12:20:31 +0530

    connection to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const connectDB = require('./database/db');
+
 dotenv.config();
+
 const app = express();
 app.use(cors());
+
+
 app.get('/', (req, res) => {
     res.json({ message: 'Hello from server!' });
 }) 
@@ -12,5 +17,6 @@ app.use('/api/users', require('./routes/user.routes'));
 app.use('/api/explore', require('./routes/explore.routes'));
 
 app.listen(5000, () => {
+    connectDB();
     console.log('Server is listening on port 5000...');
 });
